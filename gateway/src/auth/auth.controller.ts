ae27bd65377f08043d94061f948847fcc48187c9
Stanislav Kravchenko <krstaniskav2018@gmail.com>
2024-09-02 18:51:33 +0300

    Add logout method

--- gateway/src/auth/auth.controller.ts
+++ gateway/src/auth/auth.controller.ts
@@ -2,12 +2,14 @@ import {
   Body,
   Controller,
   Post,
+  Req,
   Res,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { Response } from 'express';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +23,14 @@ export class AuthController {
     const response = await this.authService.validateUser(authDto, res);
     if (!response) throw new UnauthorizedException();
     return response;
+  }
+
+  @Post('logout')
+  logout(@Res() res: Response) {
+    if (!res.cookie) {
+      return 'You are not logged in to log out';
+    }
+    res.cookie('token', '', { expires: new Date(0), httpOnly: true });
+    return res.status(200).json({ message: 'Successfully logged out' });
   }
 }
